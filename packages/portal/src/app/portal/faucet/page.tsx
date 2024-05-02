0c1159d82988f213bceeb7b45390f0bd90b8cebe
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-05-02 13:59:44 -0700

    add seo headers

--- packages/portal/src/app/portal/faucet/page.tsx
+++ packages/portal/src/app/portal/faucet/page.tsx
@@ -1,10 +1,26 @@
 import { DashLayout } from "@/components/ui/dash-layout";
 import StepsForm from "@/app/portal/faucet/steps-form";
-import {CardDescription, CardTitle} from "@/components/ui/card";
+import { CardDescription, CardTitle } from "@/components/ui/card";
+import { SeoHead } from "@/components/seo-head";
+import {Metadata} from "next";
+
+const cfg = {
+  title: 'Faucet - Cuckoo Portal',
+  site: "@CuckooNetworkHQ",
+  images: ["https://cuckoo-network.b-cdn.net/cuckoo-social-card.webp"],
+  description: "Complete a series of steps below to get CUC. Cuckoo Portal is the management portal for Cuckoo decentralized AI platform",
+};
+
+export const metadata: Metadata = {
+  title: cfg.title,
+  twitter: cfg,
+  openGraph: cfg,
+}
 
 export default function FaucetPage() {
   return (
     <DashLayout>
+      <SeoHead />
       <div className="flex flex-col">
         <CardTitle className="text-lg font-semibold md:text-2xl">
           Faucet
@@ -14,8 +30,7 @@ export default function FaucetPage() {
         </CardDescription>
       </div>
 
-      <StepsForm/>
-
+      <StepsForm />
     </DashLayout>
   );
 }
--- packages/portal/src/app/portal/staking/page.tsx
+++ packages/portal/src/app/portal/staking/page.tsx
@@ -1,5 +1,19 @@
 import { DashLayout } from "@/components/ui/dash-layout";
 import { StakingHome } from "@/app/portal/staking/staking-home";
+import {Metadata} from "next";
+
+const cfg = {
+  title: 'Staking - Cuckoo Portal',
+  site: "@CuckooNetworkHQ",
+  images: ["https://cuckoo-network.b-cdn.net/cuckoo-social-card.webp"],
+  description: "Stake CUC to secure the decentralized AI Platform and get 6% yearly. Cuckoo Portal is the management portal for Cuckoo decentralized AI platform",
+};
+
+export const metadata: Metadata = {
+  title: cfg.title,
+  twitter: cfg,
+  openGraph: cfg,
+}
 
 export default function StakingPage() {
   return (
