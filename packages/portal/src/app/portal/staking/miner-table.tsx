b050b8805b8cc38a0b899179a714e3bb011e4d53
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-05-01 20:24:30 -0700

    sort miners

--- packages/portal/src/app/portal/staking/miner-table.tsx
+++ packages/portal/src/app/portal/staking/miner-table.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
-import {GPUList} from "@/app/portal/staking/gpu-list";
-import {VoteButton} from "@/app/portal/staking/vote-button";
-import {useFetchGPUProviders} from "@/app/portal/staking/hooks/use-fetch-gpu-providers";
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { GPUList } from "@/app/portal/staking/gpu-list";
+import { VoteButton } from "@/app/portal/staking/vote-button";
+import { useFetchGPUProviders } from "@/app/portal/staking/hooks/use-fetch-gpu-providers";
+import { CardDescription, CardTitle } from "@/components/ui/card";
 
 interface CPUInfo {
   "count logical": number;
@@ -22,9 +30,9 @@ interface Miner {
 }
 
 function parseRAMUsage(used: string, total: string): string {
-  const usedGB = parseFloat(used.replace('GB', ''));
-  const totalGB = parseFloat(total.replace('GB', ''));
-  return (usedGB / totalGB * 100).toFixed(2) + '%';
+  const usedGB = parseFloat(used.replace("GB", ""));
+  const totalGB = parseFloat(total.replace("GB", ""));
+  return ((usedGB / totalGB) * 100).toFixed(2) + "%";
 }
 
 function shortenAddress(address: string): string {
@@ -35,59 +43,73 @@ export const MinerTable: React.FC = () => {
   const { providers: miners, isLoading } = useFetchGPUProviders();
 
   if (isLoading) {
-    return <></>
+    return <></>;
   }
 
   return (
-  <>
-    <h2>Miners</h2>
-    <Table>
-      <p>Votes below are calculated every 30 Minutes.</p>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Rank</TableHead>
-          <TableHead>Miner</TableHead>
-          <TableHead>Votes</TableHead>
-          <TableHead>GPUs</TableHead>
-          <TableHead>CPUs</TableHead>
-          <TableHead>RAM Usage</TableHead>
-          <TableHead>Action</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {miners.map((miner: any, index: number) => (
-          <TableRow key={miner.walletAddress}>
-            <TableCell>{index + 1}</TableCell>
-            <TableCell>
-                            <span
-                              title={miner.walletAddress}
-                              onClick={() => copyToClipboard(miner.walletAddress)}
-                              style={{cursor: 'pointer', textDecoration: 'underline'}}
-                            >
-                {shortenAddress(miner.walletAddress)}
-              </span>
+    <div className="flex flex-col">
+      <CardTitle className="text-lg font-semibold md:text-2xl">
+        Miners
+      </CardTitle>
+      <CardDescription>
+        Votes below are calculated every 30 Minutes.
+      </CardDescription>
 
-            </TableCell>
-            <TableCell>{(BigInt(miner.votes) / BigInt("1000000000000000000")).toString()}</TableCell>
-            <TableCell><GPUList models={miner.nvidia_gpu_models}/></TableCell>
-            <TableCell>{miner.CPU["count logical"]}</TableCell>
-            <TableCell>{parseRAMUsage(miner.RAM.used, miner.RAM.total)}</TableCell>
-            <TableCell>
-              <VoteButton address={miner.walletAddress}/>
-            </TableCell>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Rank</TableHead>
+            <TableHead>Miner</TableHead>
+            <TableHead>Votes</TableHead>
+            <TableHead>GPUs</TableHead>
+            <TableHead>CPUs</TableHead>
+            <TableHead>RAM Usage</TableHead>
+            <TableHead>Action</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </>
-)};
+        </TableHeader>
+        <TableBody>
+          {miners
+            .sort((a: any, b: any) => b.votes - a.votes)
+            .map((miner: any, index: number) => (
+              <TableRow key={miner.walletAddress}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>
+                  <span
+                    title={miner.walletAddress}
+                    onClick={() => copyToClipboard(miner.walletAddress)}
+                    style={{ cursor: "pointer", textDecoration: "underline" }}
+                  >
+                    {shortenAddress(miner.walletAddress)}
+                  </span>
+                </TableCell>
+                <TableCell>
+                  {(
+                    BigInt(miner.votes) / BigInt("1000000000000000000")
+                  ).toString()}
+                </TableCell>
+                <TableCell>
+                  <GPUList models={miner.nvidia_gpu_models} />
+                </TableCell>
+                <TableCell>{miner.CPU["count logical"]}</TableCell>
+                <TableCell>
+                  {parseRAMUsage(miner.RAM.used, miner.RAM.total)}
+                </TableCell>
+                <TableCell>
+                  <VoteButton address={miner.walletAddress} />
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
 function copyToClipboard(text: string) {
-  const textarea = document.createElement('textarea');
+  const textarea = document.createElement("textarea");
   textarea.value = text;
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(textarea);
 }
-
