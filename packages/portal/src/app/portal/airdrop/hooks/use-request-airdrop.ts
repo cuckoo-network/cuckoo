c4a383a26916657d7e7170b3a357f9cb0705661f
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-07-25 08:27:13 -0700

    update queryAirdropStats refetch

--- packages/portal/src/app/portal/airdrop/hooks/use-request-airdrop.ts
+++ packages/portal/src/app/portal/airdrop/hooks/use-request-airdrop.ts
@@ -1,12 +1,18 @@
 import { useMutation } from "@apollo/client";
 import { mutateRequestAirdrop } from "@/app/portal/airdrop/data/mutations";
-import { queryAirdropHistory } from "@/app/portal/airdrop/data/queries";
+import {
+  queryAirdropHistory,
+  queryAirdropStats,
+} from "@/app/portal/airdrop/data/queries";
 
 export const useRequestAirdrop = () => {
   const [requestAirdrop, { data, loading }] = useMutation(
     mutateRequestAirdrop,
     {
-      refetchQueries: [{ query: queryAirdropHistory }],
+      refetchQueries: [
+        { query: queryAirdropHistory },
+        { query: queryAirdropStats },
+      ],
     },
   );
   return {
