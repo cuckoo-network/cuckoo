2c3b44b54cb26fba8f970b5f1b10a52947ed47b7
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-08-23 10:53:53 -0700

    update

--- packages/portal/src/components/get-cai-banner.tsx
+++ packages/portal/src/components/get-cai-banner.tsx
@@ -11,6 +11,18 @@ export const GetCaiBanner = () => {
       <p>{t("components_need_more_cai")}</p>
       <div className="flex flex-row align-baseline gap-4">
         <a
+          className={"flex justify-end"}
+          target={"_blank"}
+          href={"https://ave.ai/token/0x9e3c88e95811db0db93b675f9985faff3972c615-arbitrum"}
+        >
+          <Image
+            width={240}
+            height={30}
+            src={"https://cuckoo-network.b-cdn.net/ave.ai.webp"}
+            alt={t("components_cuckoo_bridge_alt")}
+          />
+        </a>
+        <a
           target={"_blank"}
           href={
             "https://app.uniswap.org/explore/pools/arbitrum/0x7DD5e64E46b3218aa6B8327eED8fD267fcF8C10c"
