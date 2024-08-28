d81382008b92e726855c9c7ff77f222514a94d50
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-08-27 21:20:55 -0700

    Revert "Revert "Revert "Revert "fix infinite scroll in mobile width""""
    This reverts commit 298ca264b0402e32e42ef63248e817cca17b6239.

--- packages/portal/src/app/portal/art/trending-posts-masonry.tsx
+++ packages/portal/src/app/portal/art/trending-posts-masonry.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useSocialPosts } from "@/app/portal/art/hooks/use-social-posts";
 import { Button } from "@/components/ui/button";
 import { ImagePlus, Sparkles } from "lucide-react";
@@ -15,11 +15,53 @@ function selectSocialPosts(dataTrendingPosts: SocialPostsQuery | undefined) {
 
 const pageSize = 20;
 
+// Custom debounce function
+function debounce(func: Function, wait: number) {
+  let timeout: NodeJS.Timeout;
+  return function (...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
 export const TrendingPostsMasonry = () => {
   const { dataSocialPosts, loadingSocialPosts, fetchMoreSocialPosts } =
     useSocialPosts(pageSize, "0");
 
-  const postGroups = groupIntertwined(selectSocialPosts(dataSocialPosts) || []);
+  const [columns, setColumns] = useState(5);
+
+  // Debounced updateColumns function
+  const updateColumns = useCallback(
+    debounce(() => {
+      if (window.innerWidth >= 1536) {
+        setColumns(5);
+      } else if (window.innerWidth >= 1280) {
+        setColumns(5);
+      } else if (window.innerWidth >= 1024) {
+        setColumns(4);
+      } else if (window.innerWidth >= 768) {
+        setColumns(2);
+      } else {
+        setColumns(2);
+      }
+    }, 200), // Debounce delay in milliseconds
+    [],
+  );
+
+  useEffect(() => {
+    updateColumns();
+    window.addEventListener("resize", updateColumns);
+    return () => window.removeEventListener("resize", updateColumns);
+  }, [updateColumns]);
+
+  const postGroups = groupIntertwined(
+    selectSocialPosts(dataSocialPosts) || [],
+    columns,
+  );
   const endCursor = dataSocialPosts?.socialPosts.pageInfo.endCursor || "";
   const hasNext = !!dataSocialPosts?.socialPosts.pageInfo.hasNextPage;
 
@@ -34,6 +76,7 @@ export const TrendingPostsMasonry = () => {
   if (loadingSocialPosts) {
     return <></>;
   }
+
   return (
     <>
       <div className={"flex gap-2"}>
@@ -50,7 +93,7 @@ export const TrendingPostsMasonry = () => {
         </Button>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-4">
+      <div className={`grid grid-cols-${columns} gap-4`}>
         {postGroups.map((group, ii) => {
           if (ii === 0) {
             return (
@@ -58,8 +101,9 @@ export const TrendingPostsMasonry = () => {
                 <InfiniteScroll
                   dataLength={group.length}
                   next={loadMoreItems}
-                  hasMore={!!dataSocialPosts?.socialPosts.pageInfo.hasNextPage}
+                  hasMore={hasNext}
                   loader={<h4>Loading...</h4>}
+                  className="gap-4"
                 >
                   {group.map((post) => {
                     const photo = post.photoMedia?.at(0);
@@ -92,15 +136,15 @@ export const TrendingPostsMasonry = () => {
   );
 };
 
-function groupIntertwined<T>(items: T[]): T[][] {
+function groupIntertwined<T>(items: T[], numGroups: number): T[][] {
   if (!items) {
     return [[]];
   }
 
-  const groups: T[][] = [[], [], [], [], []];
+  const groups: T[][] = Array.from({ length: numGroups }, () => []);
 
   for (let i = 0; i < items.length; i++) {
-    const groupIndex = i % groups.length;
+    const groupIndex = i % numGroups;
     groups[groupIndex].push(items[i]);
   }
 
