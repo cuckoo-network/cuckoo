8661e4439fb371ab3d5c90c777d6820191e79629
Lark <165338234+larkbirdy@users.noreply.github.com>
2024-11-12 02:19:43 -0800

    Revert "update doc"
    This reverts commit b19f8de9c63fd9172693e48098688b566f785893.

--- packages/home/docusaurus.config.ts
+++ packages/home/docusaurus.config.ts
@@ -2,8 +2,6 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 import tailwindPlugin from "./plugins/tailwind-config.cjs";
-import commit from "./src/latest-commit.json";
-import { formatDistanceToNow } from "date-fns";
 
 const config: Config = {
   title: "Cuckoo AI",
@@ -356,7 +354,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Cuckoo Network (${commit.hash.slice(0, 7)} / Updated ${formatDistanceToNow(new Date(commit.date), { addSuffix: true })})`,
+      copyright: `Copyright © ${new Date().getFullYear()} Cuckoo`,
     },
 
     prism: {
