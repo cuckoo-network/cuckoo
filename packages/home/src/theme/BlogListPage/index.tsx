dc96a9ee0d4c098d89bf4342ca08c7b4d98fd754
Lark <165338234+larkbirdy@users.noreply.github.com>
2025-06-11 14:03:45 -0700

    update blog

--- packages/home/src/theme/BlogListPage/index.tsx
+++ packages/home/src/theme/BlogListPage/index.tsx
@@ -64,7 +64,7 @@ function BlogListPageContent(props: Props): JSX.Element {
             {featuredPost && (
               <div className="pb-12 md:pb-20">
                 <article className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
-                  <a
+                  <Link
                     href={featuredPost.content.metadata.permalink}
                     className="relative block group"
                     data-aos="fade-right"
@@ -88,12 +88,12 @@ function BlogListPageContent(props: Props): JSX.Element {
                         />
                       </figure>
                     )}
-                  </a>
+                  </Link>
                   <div data-aos="fade-left" data-aos-delay="200">
                     <header>
                       <h3 className="h4 mb-2">
                         <Link
-                          href={props.metadata.permalink}
+                          href={featuredPost.content.metadata.permalink}
                           className="dark:hover:text-gray-100 hover:text-gray-400 transition duration-150 ease-in-out dark:text-white"
                         >
                           {featuredPost.content.metadata.title}
